d7f72eeb771d87e14dbfe1f07f6a7dc8db0ebf23
dray-cyber <66476272+dray-cyber@users.noreply.github.com>
2020-12-01 14:23:36 -0700

    added audio capablilities

--- googlehomepush/__init__.py
+++ googlehomepush/__init__.py
@@ -20,3 +20,5 @@ class GoogleHome:
         url = u"https://translate.google.com/translate_tts?ie=UTF-8&q=" + text + "%21&tl=" + lang + "&ttsspeed=1&total=1&idx=0&client=tw-ob&textlen=14&tk=594228.1040269"
         r = req.get(url)
         self.play(url)
+    def volume(self, volume):
+        self.cc.set_volume(volume)
